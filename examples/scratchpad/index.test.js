7810e6ae54364cc27a204deae63b0d2b191d2fcd
Steve Kinney <hello@stevekinney.net>
2024-09-29 12:57:06 -0600

    Set up a scratch pad.

---
+++ examples/scratchpad/index.test.js
@@ -0,0 +1,5 @@
+import { test, expect } from 'vitest';
+
+test('a super simple test', () => {
+  expect(true).toBe(true);
+});
--- packages/css-configuration/index.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * @type import('vite').UserConfig['css']
- */
-export const css: import('vite').UserConfig['css'];
