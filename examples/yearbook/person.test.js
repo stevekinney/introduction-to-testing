252c73d651226ce35234d3e47bb0e9b0fcfbf675
Steve Kinney <hello@stevekinney.net>
2024-09-29 16:52:06 -0600

    Remove person example

--- examples/yearbook/person.test.js
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import { Person } from './person';
-
-describe('Person', () => {
-  it('should be an instance of a Person', () => {
-    const person = new Person('John', 'Doe', '2000-01-01');
-    expect(person).toBeInstanceOf(Person);
-  });
-});
