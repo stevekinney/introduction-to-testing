f3b1df89bcecb104a61376061e3ab5435a77862f
Steve Kinney <hello@stevekinney.net>
2024-09-30 13:20:40 -0600

    Remove example spy test

--- examples/logjam/src/log.test.js
+++ examples/logjam/src/log.test.js
@@ -1,14 +1,4 @@
 import { expect, it, vi, beforeEach, afterEach, describe } from 'vitest';
 import { log } from './log';
 
-describe.todo('logger', () => {
-  it('logs to the console in development mode', () => {
-    const spy = vi.spyOn(console, 'log');
-
-    log('Hello, world!');
-
-    expect(spy).toHaveBeenCalledWith('Hello, world!');
-
-    spy.mockRestore();
-  });
-});
+describe.todo('logger', () => {});
