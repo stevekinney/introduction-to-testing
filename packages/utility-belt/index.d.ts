523c05d3f562e2edd59278c6f716bf346e3c7275
Steve Kinney <hello@stevekinney.net>
2024-09-30 10:37:34 -0600

    Remove utility belt

--- packages/utility-belt/index.d.ts
+++
@@ -1,7 +0,0 @@
-export type RequireOnly<T, Keys extends keyof T = keyof T> = Partial<T> &
-  Required<Pick<T, Keys>>;
-export declare const isError: (error: unknown) => error is Error;
-export declare const getErrorMessage: (
-  error: unknown,
-  defaultMessage: string,
-) => string;
