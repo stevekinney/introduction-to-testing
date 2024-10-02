058fe1fdc8310755daa849bdd5c66180f0ee4cba
Steve Kinney <hello@stevekinney.net>
2024-10-02 12:53:23 -0500

    Update person.test.js

--- examples/characters/src/person.test.js
+++ examples/characters/src/person.test.js
@@ -2,11 +2,12 @@ import { describe, it, expect } from 'vitest';
 import { Person } from './person.js';
 
 // Remove the `todo` from the `describe` to run the tests.
-describe.todo('Person', () => {
+describe('Person', () => {
   // This test will fail. Why?
   it('should create a person with a first name and last name', () => {
     const person = new Person('Grace', 'Hopper');
     expect(person).toEqual({
+      id: expect.stringContaining('person-'),
       firstName: 'Grace',
       lastName: 'Hopper',
     });
