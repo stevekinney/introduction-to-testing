9d5615cb9d2ef239644f722e62734ba43338cd34
Steve Kinney <hello@stevekinney.net>
2024-10-02 16:27:23 -0500

    Revert "Accident counter"
    This reverts commit 9cb1a0c7395a7e15c754604eb4d6cf1c8b76293c.

--- examples/accident-counter/src/counter.test.jsx
+++ examples/accident-counter/src/counter.test.jsx
@@ -1,82 +1,34 @@
-import { render, screen, act } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { Counter } from './counter';
 
 import '@testing-library/jest-dom/vitest';
 
-describe('Counter ', () => {
-  it('renders with an initial count of 0', () => {
+describe.todo('Counter ', () => {
+  beforeEach(() => {
     render(<Counter />);
-    const counter = screen.getByTestId('counter-count');
-    expect(counter).toHaveTextContent('0');
   });
 
-  it('disables the "Decrement" and "Reset" buttons when the count is 0', () => {
-    render(<Counter />);
-    const decrementButton = screen.getByRole('button', { name: /decrement/i });
-    const resetButton = screen.getByRole('button', { name: /reset/i });
+  it('renders with an initial count of 0');
 
-    expect(decrementButton).toBeDisabled();
-    expect(resetButton).toBeDisabled();
-  });
+  it('disables the "Decrement" and "Reset" buttons when the count is 0');
 
-  it('displays "days" when the count is 0', () => {
-    render(<Counter />);
-    const unit = screen.getByTestId('counter-unit');
-    expect(unit).toHaveTextContent('days');
-  });
+  it.todo('displays "days" when the count is 0', () => {});
 
-  it('increments the count when the "Increment" button is clicked', async () => {
-    render(<Counter />);
-    const incrementButton = screen.getByRole('button', { name: /increment/i });
-    const counter = screen.getByTestId('counter-count');
+  it.todo(
+    'increments the count when the "Increment" button is clicked',
+    async () => {},
+  );
 
-    await act(async () => {
-      await userEvent.click(incrementButton);
-    });
+  it.todo('displays "day" when the count is 1', async () => {});
 
-    expect(counter).toHaveTextContent('1');
-  });
+  it.todo(
+    'decrements the count when the "Decrement" button is clicked',
+    async () => {},
+  );
 
-  it('displays "day" when the count is 1', async () => {
-    render(<Counter />);
-    const incrementButton = screen.getByRole('button', { name: /increment/i });
-    const unit = screen.getByTestId('counter-unit');
-
-    await act(async () => {
-      await userEvent.click(incrementButton);
-    });
-
-    expect(unit).toHaveTextContent('day');
-  });
-
-  it('decrements the count when the "Decrement" button is clicked', async () => {
-    render(<Counter initialCount={1} />);
-    const decrementButton = screen.getByRole('button', { name: /decrement/i });
-    const count = screen.getByTestId('counter-count');
-
-    expect(decrementButton).not.toBeDisabled();
-
-    await act(async () => {
-      await userEvent.click(decrementButton);
-    });
-
-    expect(count).toHaveTextContent('0');
-    expect(decrementButton).toBeDisabled();
-  });
-
-  it('does not allow decrementing below 0', async () => {
-    render(<Counter />);
-    const decrementButton = screen.getByRole('button', { name: /decrement/i });
-    const count = screen.getByTestId('counter-count');
-
-    await act(async () => {
-      await userEvent.click(decrementButton);
-    });
-
-    expect(count).toHaveTextContent('0');
-  });
+  it.todo('does not allow decrementing below 0', async () => {});
 
   it.todo(
     'resets the count when the "Reset" button is clicked',
@@ -88,14 +40,5 @@ describe('Counter ', () => {
     () => {},
   );
 
-  it('updates the document title based on the count', async () => {
-    const { getByRole } = render(<Counter />);
-    const incrementButton = getByRole('button', { name: /increment/i });
-
-    await act(async () => {
-      await userEvent.click(incrementButton);
-    });
-
-    expect(document.title).toEqual(expect.stringContaining('1 day'));
-  });
+  it.todo('updates the document title based on the count', async () => {});
 });
