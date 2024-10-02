579a55a4ae38cc84e9e62b1e8608292c1c7efbdd
Steve Kinney <hello@stevekinney.net>
2024-10-02 15:38:02 -0500

    Update alert-button.test.jsx

--- examples/element-factory/src/alert-button.test.jsx
+++ examples/element-factory/src/alert-button.test.jsx
@@ -3,12 +3,25 @@ import userEvent from '@testing-library/user-event';
 
 import { AlertButton } from './alert-button';
 
-describe.todo('AlertButton', () => {
+describe('AlertButton', () => {
   beforeEach(() => {});
 
   afterEach(() => {});
 
   it('should render an alert button', async () => {});
 
-  it('should trigger an alert', async () => {});
+  it.only('should trigger an alert', async () => {
+    // const logSpy = vi.spyOn(console, 'log').mockImplementation(() => {})
+
+    render(<AlertButton />);
+    const input = screen.getByLabelText('Message');
+    const button = screen.getByRole('button', { name: /trigger alert/i });
+
+    await act(async () => {
+      await userEvent.type(input, 'Hello');
+    });
+
+    // Click on the button.
+    // Look and see if alert() was called with the message.
+  });
 });
